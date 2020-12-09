b1846107d2e07cda41ef60b74f1415ccaa8fbb56
Weston Schmidt <Weston_Schmidt@alumni.purdue.edu>
2020-12-08 17:24:05 -0800

    Add some grossness.

--- main.go
+++ main.go
@@ -2,11 +2,27 @@ package main
 
 import "fmt"
 
-func foo() string {
+func foo(a, b, c int) string {
+	// TODO
+
+	if a == b {
+		if b < a {
+			if a < c {
+				if c < b {
+					if a+b-12 > 33 {
+						return "foo"
+					}
+				}
+			}
+		}
+	}
+
+	go foo(a+1, b+2, c-a)
+
 	return "Sup?"
 }
 
 func main() {
 	// I am making some changes so LGMT does something
-	fmt.Println(foo())
+	fmt.Println(foo(1, 2, 3))
 }
